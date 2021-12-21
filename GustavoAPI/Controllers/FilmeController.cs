645ca890a251fd07a196d644dc3e52ccdf1296b8
Gustavo Moreira <gustamoreira26@gmail.com>
2021-12-21 16:47:33 -0300

    Adicionar arquivos de projeto.

---
+++ GustavoAPI/Controllers/FilmeController.cs
@@ -0,0 +1,26 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GustavoAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+
+
+        private static List<Filme> filmes = new List<Filme>();
+
+        public void AdicionaFilme(Filme filme)
+        {
+            filmes.Add(filme);
+
+            Console.WriteLine(filme.Titulo);
+
+        }
+    }
+}
